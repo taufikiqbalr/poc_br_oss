db74a62866cdbac715c6aeb3f5efeaa1b828e320
Taufik Iqbal Ramdhani <taufikiqbalr@gmail.com>
2026-09-17 05:33:20 +0700

    chore: default API to port 3402

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true, credentials: false });
-  const port = Number(process.env.PORT || 3002);
+  const port = Number(process.env.PORT || 3402);
   await app.listen(port, '0.0.0.0');
   console.log(`OSS Business Rules PoC API listening on ${port}`);
 }
